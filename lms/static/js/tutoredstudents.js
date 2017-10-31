80de0821f8612bfbe08ad247a40b37f7f3741398
nakita <nakitamccool@gmail.com>
2017-10-31 14:18:36 +0000

    Added SB and tutor pilot students joining the new LP

--- lms/static/js/tutoredstudents.js
+++ lms/static/js/tutoredstudents.js
@@ -5,11 +5,12 @@ var TUTORED_STUDENTS = {
     'nakita_student': {id: 2267165000006727085, email: 'nakitacodeinstitute@gmail.com'},
     'tiffanysnell': {id: 1267165000006727085, email: 'tiffany@codeinstitute.net'},
     'snig501': {id: 3267165000006727085, email: 'aaron@codeinstitute.net'},
-    // actual students
+    // DIRECT
     'tjasa_jan': {id: 1267165000015434169, email: 'tjasaa@gmail.com'},
     'Bruce_H': {id: 1267165000014948209, email: 'bruce@harrisonemail.com'},
     'denismoc': {id: 1267165000014520506, email: 'denismoc@live.ie'},
     'eloisestancioff': {id: 1267165000014554557, email: 'cestancioff@gmail.com'},
+    // July SB
     'danielmontanari': {id: 1267165000012613686, email: 'daniel_montanari@hotmail.com'},
     'deankelly': {id: 1267165000012554810, email: 'Dean.kelly07@gmail.com'},
     'libusehorankova': {id: 1267165000012554134, email: 'libusehoran@gmail.com'},
@@ -26,4 +27,44 @@ var TUTORED_STUDENTS = {
     'piotrszelest': {id: 1267165000012241665, email: 'p.szelest@live.com'},
     'johnburns': {id: 1267165000012241643, email: 'mrjohnburns@gmail.com'},
     'patricklinnane': {id: 1267165000012977229, email: 'patrickjosephlinnane@gmail.com'},
+    // Sept SB
+    'aislingodonovan': {id: 1267165000013132546, email: 'aisling1odonovan@gmail.com'},
+    'alanmurphy': {id: 1267165000014709477, email: 'alanjmurphy91@gmail.com'},
+    'amymoriarty': {id: 1267165000013332034, email: 'lostjrt16@gmail.com'},
+    'andrewhill': {id: 1267165000014223164, email: 'leftism73@gmail.com'},
+    'athanasiosplatsas': {id: 1267165000014665742, email: 'sakis.platsas@gmail.com'},
+    'baileycrowley123': {id: 1267165000014223057, email: 'baileycrowley123@gmail.com'},
+    'brendankelly': {id: 1267165000014001013, email: 'brendankelly20@gmail.com'},
+    'brianpope': {id: 1267165000014702785, email: 'im.buoyant@gmail.com'},
+    'caitrionalangan': {id: 1267165000013241049, email: 'trionalangan@me.com'},
+    'ciarankehoe': {id: 1267165000014001917, email: 'ciarankehoe22@gmail.com'},
+    'clivenoonan': {id: 1267165000014001969, email: 'clivenoonan93@gmail.com'},
+    'damiankuchta': {id: 1267165000014706337, email: 'damkuch@gmail.com'},
+    'dylankershaw': {id: 1267165000013482486, email: 'Dylandkershaw@gmail.com'},
+    'eamonpender': {id: 1267165000014001722, email: 'eamon.pender@gmail.com'},
+    'eamonnryan': {id: 1267165000013482029, email: 'globe3d@gmail.com'},
+    'emanuelbaran': {id: 1267165000014706387, email: 'baran-emanuel@hotmail.com'},
+    'fintanconway': {id: 1267165000013522095, email: 'waycontec@gmail.com'},
+    'gearoidheaney': {id: 1267165000013522117, email: 'gearoidheaney@hotmail.com'},
+    'maireadmccarthy': {id: 1267165000014480010, email: 'maireadmccarthyster@gmail.com'},
+    'marcdelarago': {id: 1267165000013924306, email: 'earth@live.ie'},
+    'markcooney': {id: 1267165000014104251, email: 'auchincloss1990@gmail.com'},
+    'markauchincloss': {id: 1267165000014001441, email: 'markcooney13@gmail.com'},
+    'neillmerrick': {id: 1267165000014188071, email: 'neillmerrick@gmail.com'},
+    'tommywalsh': {id: 1267165000013924007, email: 'tommywalsh1984@gmail.com'},
+    'vadymkalinin': {id: 1267165000013969081, email: 'vadym.kalinin@hotmail.com'},
+    // Tutor Pilot
+    'mirkapavelkova': {id: 1267165000005674061, email: 'miroslava.pavelkova@yahoo.ie'},
+    'melaniesmollett': {id: 1267165000009553912, email: 'melaniesmollett@gmail.com'},
+    'habtemariamyohannes': {id: 1267165000011557100, email: 'hyohannes@hotmail.com'},
+    'willrussell': {id: 1267165000011614097, email: 'russellgwill@gmail.com'},
+    'markharrison': {id: 1267165000008052045, email: 'mharrisondesign@mail.com'},
+    'charlesthompson': {id: 1267165000007058030, email: 'charles.thompson@ctclimited.net'}
 };
+
+
+
+
+  
+
+
