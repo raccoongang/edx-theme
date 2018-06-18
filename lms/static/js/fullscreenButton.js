b5779239b720691a020f75518b67dd2cd475b557
Misha Malyavin <suxx_2000@hotmail.com>
2018-06-18 16:42:13 +0300

    added svg sprite

--- lms/static/js/fullscreenButton.js
+++ lms/static/js/fullscreenButton.js
@@ -1,24 +1,28 @@
 $(function () {
     let fs = document.getElementById('fullscreen');
     let main = document.getElementById('course-content');
+
     function toggleFullScreen() {
         let fullscreen = document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement;
+        console.log(fs);
         if (!fullscreen) {
-            fs.classList.remove('fa-arrows-alt');
-            fs.classList.add('fa-compress');
             if (main.msRequestFullscreen) {main.msRequestFullscreen()}
             if (main.webkitRequestFullscreen) {main.webkitRequestFullscreen()}
             if (main.mozRequestFullScreen) {main.mozRequestFullScreen()}
+            cahngeXlink('#ico-fs-shrink');
         } else {
-            fs.classList.remove('fa-compress');
-            fs.classList.add('fa-arrows-alt');
             if(document.msExitFullscreen) {document.msExitFullscreen()}
             if(document.mozCancelFullScreen) {document.mozCancelFullScreen()}
             if(document.webkitExitFullscreen) {document.webkitExitFullscreen()}
+            cahngeXlink('#ico-fs-expand');
         }
     }
 
-    fs.addEventListener("click", function(e) {
+    let cahngeXlink = (val) => {
+        document.querySelector('use').setAttributeNS('http://www.w3.org/1999/xlink', 'href', val);
+    };
+
+    fs.addEventListener('click', () => {
         toggleFullScreen();
-    }, false);
-})
+    })
+});
