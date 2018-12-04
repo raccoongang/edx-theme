bef70b4cfa949c7c82f8988b4b835110a3539f98
lunyachek <ananasgo@gmail.com>
2018-12-04 23:15:48 +0200

    ntnu-9 theme start

--- lms/static/js/slider.js
+++ lms/static/js/slider.js
@@ -1,15 +1,16 @@
 $(window).load(function () {
-    $('section.home > .slider').slick({
+    $('.main-slider').slick({
         dots: true,
+        arrows: false,
         infinite: true,
-        speed: 700,
-        fade: true,
-        cssEase: 'linear',
-        autoplay: true,
-        autoplaySpeed: 7000
+        centerMode: true,
+        variableWidth: true,
+        draggable: false
+        // slidesToShow: 3,
+        // slidesToScroll: 1
     });
 });
 
-$(window).on('resize orientationchange', function () {
-    $('section.home > .slider').slick('resize');
-});
+// $(window).on('resize orientationchange', function () {
+//     $('section.home > .slider').slick('resize');
+// });
