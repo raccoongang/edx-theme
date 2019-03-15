90350e7b3744b2388fdeaa0967655a96c0599137
ihor-romaniuk <Igorek25>
2019-03-15 10:59:54 +0200

    [BI-219] hide course dropdown menu after click outside

--- lms/static/js/common.js
+++ lms/static/js/common.js
@@ -15,4 +15,24 @@ $(document).ready(function () {
     }
   });
 
+  // hide course drop down menu (on the dashboard) on click outside
+  $(document).on('click', function (e) {
+    if($('.actions-dropdown.is-visible').length) {
+      var notCurrentDropdown = $('.action-more').not($(e.target)[0]).next();
+
+      if(notCurrentDropdown.hasClass('is-visible')) {
+        notCurrentDropdown.removeClass('is-visible');
+        notCurrentDropdown.attr('tabindex', -1);
+      }
+
+      if (jQuery(e.target).closest('.wrapper-action-more').length) {
+        return;
+      }
+
+      var dropdown = $('.actions-dropdown');
+      dropdown.removeClass('is-visible');
+      dropdown.attr('tabindex', -1);
+    }
+  });
+
 });
