badb8197eeb691add1806a5d452a91c252cc3829
vlasovmichael <vlasovmichael88@gmail.com>
2020-03-03 19:28:11 +0200

    [SKILLONOMY-186] Course info page update
    * enroll/unenroll button deleted (as customer asked)

--- lms/static/js/main.js
+++ lms/static/js/main.js
@@ -36,5 +36,45 @@ $(document).ready(function () {
         transition: 'all 0.3s',
         scrolllock: true
     });
+    // open/close course program on the info page
+    $('.week-structure__name').on('click', function() {
+        var self = $(this);
 
+        self.toggleClass('is-active');
+        self.parent().find('.week-structure__box').slideToggle();
+    });
+    // show/hide course program on the info page
+    $('.js-course-program').on('click', function() {
+        $('#main > .container').toggleClass('panel-is-hide');
+    });
+
+    // referral btn on the info page
+    $('.js-referral-btn').on('click', function() {
+        var self = $(this);
+        var value = self.data('clipboard-text'); 
+        var $temp = $("<input>");
+
+        self.addClass('is-copied');
+        setTimeout(function() {
+            self.removeClass('is-copied');
+        }, 3000);
+        // copy to clipbord
+        $("body").append($temp);
+        $temp.val(value).select();
+        document.execCommand("copy");
+        $temp.remove();
+    });
+
+    // mobile menu tabs
+    $('.js-tabs-mobile-btn').on('click', function() {
+        $(this).toggleClass('is-open');
+        $('#course__tabs').toggleClass('is-visible');
+    });
+
+    $(document).on('click touchstart', function(event) {
+        if (!$(event.target).closest('.course-material').length) {
+            $('#course__tabs').removeClass('is-visible');
+            $('.js-tabs-mobile-btn').removeClass('is-open');
+        }
+    });
 });
