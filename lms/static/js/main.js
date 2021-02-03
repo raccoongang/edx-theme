86dcd16b552933477ba6fe8c3a269390f68c8058
vlasovmichael <mykhailo.vlasov@raccoongang.com>
2021-02-03 17:18:50 +0200

    [MAMAEdu-122767] Added slider to the index page for the partners section / review

--- lms/static/js/main.js
+++ lms/static/js/main.js
@@ -1,20 +1,4 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-        dots: true,
-        autoplay: true,
-        center: true,
-        loop: true,
-        margin: 15,
-        items: 2,
-        responsive: {
-            0: {
-                items: 1
-            },
-            901: {
-                items: 2
-            }
-        }
-    });
     // course page accordion
     $('.course-program-list__head').on('click', function() {
         var $answer = $(this).next();
